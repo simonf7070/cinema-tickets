76858e428e22a1f9b12b79ab1b20a954cd2cf0b7
Simon Felix <simonf7070@gmail.com>
2023-10-04 13:46:03 +0100

    extract common test setup

--- src/test/java/TicketCalculatorTests.java
+++ src/test/java/TicketCalculatorTests.java
@@ -1,5 +1,6 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import uk.gov.dwp.uc.pairtest.TicketCalculator;
@@ -8,19 +9,23 @@ import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest.Type;
 
 public class TicketCalculatorTests {
+    private TicketCalculator ticketCalculator;
+
+    @Before
+    public void setup() {
+        ticketCalculator = new TicketCalculatorImpl();
+    }
+
     @Test
     public void getNumberOfSeats_should_be_zero_when_none_requested() {
-        TicketCalculator ticketCalculator = new TicketCalculatorImpl();
-        
         assertEquals(0, ticketCalculator.getNumberOfSeats());
     }
 
     @Test
     public void getNumberOfSeats_should_be_zero_when_zero_adult_requested() {
-        TicketCalculator ticketCalculator = new TicketCalculatorImpl();
         var tickets = new TicketTypeRequest(Type.ADULT, 0);
 
         assertEquals(0, ticketCalculator.getNumberOfSeats(tickets));
-    }      
-   
+    }  
+ 
 }
