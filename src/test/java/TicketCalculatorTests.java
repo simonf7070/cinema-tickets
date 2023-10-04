884084813fadb43a79d3a82b1a10523dc83c49bb
Simon Felix <simonf7070@gmail.com>
2023-10-04 13:41:48 +0100

    add additional test for quantity of zero tickets

--- src/test/java/TicketCalculatorTests.java
+++ src/test/java/TicketCalculatorTests.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import uk.gov.dwp.uc.pairtest.TicketCalculator;
 import uk.gov.dwp.uc.pairtest.TicketCalculatorImpl;
+import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
+import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest.Type;
 
 public class TicketCalculatorTests {
     @Test
@@ -12,4 +14,13 @@ public class TicketCalculatorTests {
         
         assertEquals(0, ticketCalculator.getNumberOfSeats());
     }
+
+    @Test
+    public void getNumberOfSeats_should_be_zero_when_zero_adult_requested() {
+        TicketCalculator ticketCalculator = new TicketCalculatorImpl();
+        var tickets = new TicketTypeRequest(Type.ADULT, 0);
+
+        assertEquals(0, ticketCalculator.getNumberOfSeats(tickets));
+    }      
+   
 }
