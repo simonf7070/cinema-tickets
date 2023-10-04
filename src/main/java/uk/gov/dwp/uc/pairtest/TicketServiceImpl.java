f2abef7f0a588b93089beaccd116827150b7dfea
Simon Felix <simonf7070@gmail.com>
2023-10-04 07:58:30 +0100

    make test pass by throwing exception when invalid accountId

--- src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
+++ src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
@@ -10,7 +10,7 @@ public class TicketServiceImpl implements TicketService {
 
     @Override
     public void purchaseTickets(Long accountId, TicketTypeRequest... ticketTypeRequests) throws InvalidPurchaseException {
-
+        throw new InvalidPurchaseException("Invalid Account Id");
     }
 
 }
